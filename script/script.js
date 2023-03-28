0b5946460a6ec0228457951535740d7ebce27f21
Murad <agamedov94@mail.ru>
2023-03-28 23:34:32 +0400

    res

---
+++ script/script.js
@@ -0,0 +1,181 @@
+
+$('.slider').slick({
+   infinite: true,
+   slidesToShow: 2,
+   centerMode: false,
+   slidesToScroll: 1,
+   dots: true,
+   prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><i class="fa fa-angle-left"></i></button>',
+   nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><i class="fa fa-angle-right"></i></button>',
+   responsive: [
+      {
+        breakpoint: 788,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+ });
+
+ $('.meet__slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  centerMode: false,
+  slidesToScroll: 3,
+  dots: true,
+  prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><i class="fa fa-angle-left"></i></button>',
+  nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><i class="fa fa-angle-right"></i></button>',
+  responsive: [
+     {
+       breakpoint: 788,
+       settings: {
+         slidesToShow: 1,
+         arrows: false
+       }
+     },
+     {
+       breakpoint: 1200,
+       settings: {
+         slidesToShow: 2,
+         arrows: false
+       }
+     },
+     {
+      breakpoint: 1280,
+      settings: {
+        arrows: false
+      }
+    }
+   ]
+});
+
+
+$('.podcasts__slider').slick({
+  infinite: true,
+  slidesToShow: 4,
+  centerMode: false,
+  slidesToScroll: 4,
+  dots: true,
+  prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><i class="fa fa-angle-left"></i></button>',
+  nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><i class="fa fa-angle-right"></i></button>',
+  responsive: [
+    {
+
+      breakpoint: 600,
+      settings: {
+        dots: false,
+        slidesToShow: 1,
+        arrows: false
+      }
+    },
+     {
+       breakpoint: 788,
+       settings: {
+         dots: false,
+         slidesToShow: 2,
+         arrows: false
+       }
+     },
+     {
+       breakpoint: 1200,
+       settings: {
+         dots: false,
+         slidesToShow: 3,
+         arrows: false
+       }
+     },
+     {
+      breakpoint: 1290,
+      settings: {
+        dots: false,
+        slidesToShow: 3,
+        arrows: false
+      }
+    }
+   ]
+});
+
+
+
+
+
+
+
+
+
+
+$('.menu__mini').slick({
+  infinite: true,
+  slidesToShow: 4,
+  centerMode: false,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  
+  responsive: [
+    {
+      breakpoint: 952,
+      settings: {
+        slidesToShow: 2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 675,
+      settings: {
+        slidesToShow: 1,
+        arrows: false
+      }
+    }
+   ]
+});
+
+
+$('.our__djs__carusel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  centerMode: false,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><i class="fa fa-angle-left"></i></button>',
+  nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><i class="fa fa-angle-right"></i></button>',
+  
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 1280,
+      settings: {
+        
+        arrows: false
+      }
+    }
+   ]
+});
